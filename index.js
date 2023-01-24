9f8d37c3038bb478af5116108d4f8b98156118db
unknown <mwgoodwin2000@gmail.com>
2023-01-23 19:49:22 -0700

    adding basic code and then creating a branch

---
+++ index.js
@@ -0,0 +1,61 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a brief description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide instructions for installation:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide an example of how to use your project:'
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'List any credits or sources (if any):'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  }
+];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+inquirer.prompt(questions).then((answers) => {
+    let readme = `# ${answers.projectName}\n\n`;
+    readme += `## Description\n${answers.description}\n\n`;
+    readme += `## Installation\n${answers.installation}\n\n`;
+    readme += `## Usage\n${answers.usage}\n\n`;
+    readme += `## Credits\n${answers.credits}\n\n`;
+    readme += `## License\nThis project is licensed under the ${answers.license} license.`;
+  
+    fs.writeFileSync('README.md', readme);
+  });
+// TODO: Create a function to initialize app
+function init(fileName = "README.md") {}
+
+// Function call to initialize app
+init();
